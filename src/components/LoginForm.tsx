cee899b6d1e395b0ebd28ba8f4831d1d19a7c0bc
Tatjana Zinko <ztvproweb@gmailcom>
2023-03-05 16:54:59 +0200

    Validation

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -20,6 +20,12 @@ const Input = styled.input`
     margin-bottom: 20px;
     padding: 10px;
 `;
+const ErrorMassage = styled.p`
+    color: red;
+    font-size: 0.8rem;
+    margin: 0;
+`;
+
 
 const Button = styled.button`
     padding: 10px;
@@ -27,7 +33,13 @@ const Button = styled.button`
 
 export default function LoginForm() {
     const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
+    const [loginDirty, setloginDirty] = useState(false);
+    const [loginError, setLoginError] = useState("");
+
+    const [password, setPassword] = useState('');    
+    const [passwordDirty, setPasswordDirty] = useState(false);
+    const [passwordError, setPasswordError] = useState("");
+
     const navigate = useNavigate();
 
     const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
@@ -42,14 +54,50 @@ export default function LoginForm() {
         } else {
             localStorage.setItem("userToken", "false");
         }       
-    }    
+    }  
+    
+    const handleBlur = (e: { target: { name: string; }; }) => {
+        switch (e.target.name) {
+            case "login": 
+                setloginDirty(true);
+                break;
+            case "password": 
+                setPasswordDirty(true);
+                break;
+        }
+    }
+
+    const handleLogin = (e: { target: { value: string; }; }) => {
+        setLogin(e.target.value);
+        const reLogin = /^[A-Za-z0-9]{3,16}$/;
+        if(!reLogin.test(String(e.target.value))) {
+            setLoginError("Login should be 3-16 characters");
+        } else {
+            setLoginError("");
+        }
+    }
+
+    const handlePassword = (e: { target: { value: string; }; }) => {
+        setPassword(e.target.value);
+        const rePassword = /^[A-Za-z0-9]{8,20}$/;
+        if(!rePassword.test(String(e.target.value))) {
+            setPasswordError("Password should be 8-20 letters or numbers");
+            if(!e.target.value) {
+                setPasswordError("Password shouldn't be empty");
+            }
+        }else {
+            setPasswordError("");
+        }
+    }
 
   return (
     <Wrapper>
         <h1>Login</h1>
       <Form onSubmit={handleSubmit}>
-        <Input value={login} onChange={(e) => setLogin(e.target.value)} type="text" placeholder="Login" id="login" name="login"></Input>
-        <Input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password"></Input>
+        {(loginDirty && loginError) && <ErrorMassage>{loginError}</ErrorMassage>}
+        <Input value={login} onChange={handleLogin} onBlur={handleBlur} type="text" placeholder="Login" id="login" name="login" required></Input>
+        {(passwordDirty && passwordError) && <ErrorMassage>{passwordError}</ErrorMassage>}
+        <Input value={password} onChange={handlePassword} onBlur={handleBlur} type="password" placeholder="Password" id="password" name="password" required></Input>        
         <Button type="submit">Log in</Button>
       </Form>
     </Wrapper>
